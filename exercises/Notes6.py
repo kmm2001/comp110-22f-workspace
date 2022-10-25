2dbf1a672facf2fbf8b0592dec5026cc2e14622c
Kathleen Moskalik <moskalik@live.unc.edu>
2022-10-24 21:35:30 -0400

    Notes6 final

---
+++ exercises/Notes6.py
@@ -0,0 +1,22 @@
+#forest: str = str("The forest is large and dark. You encounter a bear! Choose how much damage to do to the bear!")
+    #cave: str = str("You enter a large, dark cave. You see a tunnel.")
+    #leave: str = str("You have exited the game. Goodbye.")
+
+#    while points > 0:
+
+
+#def fight() -> str:
+    #"You choose fight."
+    #outcome = str("You scared the bear off! You're alive!")
+    #return outcome
+
+
+#def flight() -> str:
+    #"You choose flight."
+    #outcome = str("The bear thought of you as prey and attacked. You died.")
+    #return outcome
+
+def quit_function() -> int:
+    """End the game."""
+    global points
+    return points
