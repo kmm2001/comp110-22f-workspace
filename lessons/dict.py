0bb85647bf14859f3a844ec5b0504fb51a35e300
Kathleen Moskalik <moskalik@live.unc.edu>
2022-10-24 21:32:35 -0400

    dict final

---
+++ lessons/dict.py
@@ -0,0 +1,18 @@
+# print(schools["UNCC"])
+
+schools = dict[str, int]
+schools = dict()
+schools["UNC"] = 19400
+schools["Duke"] = 6717
+schools["NCSU"] = 26150
+
+# Example looping over the keys of a dict
+#for key in schools:
+    #print(f"Key: {key} -> Value: {schools[key]}")
+
+#for school in schools:
+    #print(f"Key: {school} -> Value: {schools[school]}")
+
+
+print(type("UNC"))
+print(type(schools["UNC"]))
