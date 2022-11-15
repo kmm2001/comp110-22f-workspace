8ec78761664547bd301322a7246a36e0963ed81c
Kathleen Moskalik <moskalik@live.unc.edu>
2022-11-15 10:19:52 -0500

    constants final

--- exercises/ex09/constants.py
+++ exercises/ex09/constants.py
@@ -11,5 +11,12 @@ MIN_Y: float = -MAX_Y
 VIEW_HEIGHT: int = BOUNDS_HEIGHT + 20
 
 CELL_RADIUS: int = 15
-CELL_COUNT: int = 1
-CELL_SPEED: float = 5.0
+CELL_COUNT: int = 5
+CELL_SPEED: float = 3.0
+VULNERABLE: int = 0
+INFECTED: int = 1
+IMMUNE: int = -1
+RECOVERY_PERIOD: int = 90
+
+START_IMMUNE: int = 2
+START_INFECTED: int = 2
